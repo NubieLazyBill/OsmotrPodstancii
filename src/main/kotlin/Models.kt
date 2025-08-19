a6e4b61b89a97c629d237b269293c931c653a341
Lazy Bill <lazybill2580@gmail.com>
2025-08-19 19:37:16 +0500

    добавлено оборудование в ору-220

--- src/main/kotlin/Models.kt
+++ src/main/kotlin/Models.kt
@@ -63,6 +63,260 @@ object SubstationData {
         )
     )
 
+    val oru220 = Oru(
+        voltage = "220",
+        name = "ОРУ-220",
+        equipments = listOf(
+            //Мирная
+            Equipment(
+                id = "В-220 Мирная",
+                name = "В-220 Мирная",
+                type = EquipmentType.CIRCUIT_BREAKER,
+                parameters = listOf(
+                    InspectionParameter("Уровень продувки ф.А", "", "0.5-1"),
+                    InspectionParameter("Уровень продувки ф.В", "", "0.5-1"),
+                    InspectionParameter("Уровень продувки ф.С", "", "0.5-1"),
+                )
+            ),
+            Equipment(
+                id = "ТТ-220 Мирная",
+                name = "ТТ-220 Мирная",
+                type = EquipmentType.CURRENT_TRANSFORMER,
+                parameters = listOf(
+                    InspectionParameter("Уровень масла ф.А", "", ""),
+                    InspectionParameter("Уровень масла ф.В", "", ""),
+                    InspectionParameter("Уровень масла ф.С", "", ""),
+                )
+            ),
+            //Топаз
+            Equipment(
+                id = "В-220 Топаз",
+                name = "В-220 Топаз",
+                type = EquipmentType.CIRCUIT_BREAKER,
+                parameters = listOf(
+                    InspectionParameter("Уровень продувки ф.А", "", "0.5-1"),
+                    InspectionParameter("Уровень продувки ф.В", "", "0.5-1"),
+                    InspectionParameter("Уровень продувки ф.С", "", "0.5-1"),
+                )
+            ),
+            Equipment(
+                id = "ТТ-220 Топаз",
+                name = "ТТ-220 Топаз",
+                type = EquipmentType.CURRENT_TRANSFORMER,
+                parameters = listOf(
+                    InspectionParameter("Уровень масла ф.А", "", ""),
+                    InspectionParameter("Уровень масла ф.В", "", ""),
+                    InspectionParameter("Уровень масла ф.С", "", ""),
+                )
+            ),
+            //ОВ-220
+            Equipment(
+                id = "ОВ-220",
+                name = "ОВ-220",
+                type = EquipmentType.CIRCUIT_BREAKER,
+                parameters = listOf(
+                    InspectionParameter("Уровень продувки ф.А", "", "0.5-1"),
+                    InspectionParameter("Уровень продувки ф.В", "", "0.5-1"),
+                    InspectionParameter("Уровень продувки ф.С", "", "0.5-1"),
+                )
+            ),
+            Equipment(
+                id = "ТТ-220 ОВ",
+                name = "ТТ-220 ОВ",
+                type = EquipmentType.CURRENT_TRANSFORMER,
+                parameters = listOf(
+                    InspectionParameter("Уровень масла ф.А", "", ""),
+                    InspectionParameter("Уровень масла ф.В", "", ""),
+                    InspectionParameter("Уровень масла ф.С", "", ""),
+                )
+            ),
+            //В-220 2АТГ
+            Equipment(
+                id = "В-220 2АТГ",
+                name = "В-220 2АТГ",
+                type = EquipmentType.CIRCUIT_BREAKER,
+                parameters = listOf(
+                    InspectionParameter("Уровень продувки ф.А", "", "0.5-1"),
+                    InspectionParameter("Уровень продувки ф.В", "", "0.5-1"),
+                    InspectionParameter("Уровень продувки ф.С", "", "0.5-1"),
+                )
+            ),
+            Equipment(
+                id = "ТТ-220 2АТГ",
+                name = "ТТ-220 2АТГ",
+                type = EquipmentType.CURRENT_TRANSFORMER,
+                parameters = listOf(
+                    InspectionParameter("Уровень масла ф.А", "", ""),
+                    InspectionParameter("Уровень масла ф.В", "", ""),
+                    InspectionParameter("Уровень масла ф.С", "", ""),
+                )
+            ),
+            //ШСВ-220
+            Equipment(
+                id = "ШСВ-220",
+                name = "ШСВ-220",
+                type = EquipmentType.CIRCUIT_BREAKER,
+                parameters = listOf(
+                    InspectionParameter("Уровень продувки ф.А", "", "0.5-1"),
+                    InspectionParameter("Уровень продувки ф.В", "", "0.5-1"),
+                    InspectionParameter("Уровень продувки ф.С", "", "0.5-1"),
+                )
+            ),
+            Equipment(
+                id = "ТТ-220 ШСВ",
+                name = "ТТ-220 ШСВ",
+                type = EquipmentType.CURRENT_TRANSFORMER,
+                parameters = listOf(
+                    InspectionParameter("Уровень масла ф.А", "", ""),
+                    InspectionParameter("Уровень масла ф.В", "", ""),
+                    InspectionParameter("Уровень масла ф.С", "", ""),
+                )
+            ),
+            //В-220 3АТГ
+            Equipment(
+                id = "В-220 3АТГ",
+                name = "В-220 3АТГ",
+                type = EquipmentType.CIRCUIT_BREAKER,
+                parameters = listOf(
+                    InspectionParameter("Уровень продувки ф.А", "", "0.5-1"),
+                    InspectionParameter("Уровень продувки ф.В", "", "0.5-1"),
+                    InspectionParameter("Уровень продувки ф.С", "", "0.5-1"),
+                )
+            ),
+            Equipment(
+                id = "ТТ-220 3АТГ",
+                name = "ТТ-220 3АТГ",
+                type = EquipmentType.CURRENT_TRANSFORMER,
+                parameters = listOf(
+                    InspectionParameter("Уровень масла ф.А", "", ""),
+                    InspectionParameter("Уровень масла ф.В", "", ""),
+                    InspectionParameter("Уровень масла ф.С", "", ""),
+                )
+            ),
+            //Орбита
+            Equipment(
+                id = "В-220 Орбита",
+                name = "В-220 Орбита",
+                type = EquipmentType.CIRCUIT_BREAKER,
+                parameters = listOf(
+                    InspectionParameter("Уровень продувки ф.А", "", "0.5-1"),
+                    InspectionParameter("Уровень продувки ф.В", "", "0.5-1"),
+                    InspectionParameter("Уровень продувки ф.С", "", "0.5-1"),
+                )
+            ),
+            Equipment(
+                id = "ТТ-220 Орбита",
+                name = "ТТ-220 Орбита",
+                type = EquipmentType.CURRENT_TRANSFORMER,
+                parameters = listOf(
+                    InspectionParameter("Уровень масла ф.А", "", ""),
+                    InspectionParameter("Уровень масла ф.В", "", ""),
+                    InspectionParameter("Уровень масла ф.С", "", ""),
+                )
+            ),
+            //Факел
+            Equipment(
+                id = "В-220 Факел",
+                name = "В-220 Факел",
+                type = EquipmentType.CIRCUIT_BREAKER,
+                parameters = listOf(
+                    InspectionParameter("Уровень продувки ф.А", "", "0.5-1"),
+                    InspectionParameter("Уровень продувки ф.В", "", "0.5-1"),
+                    InspectionParameter("Уровень продувки ф.С", "", "0.5-1"),
+                )
+            ),
+            Equipment(
+                id = "ТТ-220 Факел",
+                name = "ТТ-220 Факел",
+                type = EquipmentType.CURRENT_TRANSFORMER,
+                parameters = listOf(
+                    InspectionParameter("Уровень масла ф.А", "", ""),
+                    InspectionParameter("Уровень масла ф.В", "", ""),
+                    InspectionParameter("Уровень масла ф.С", "", ""),
+                )
+            ),
+            //Комета-2
+            Equipment(
+                id = "В-220 Комета-2",
+                name = "В-220 Комета-2",
+                type = EquipmentType.CIRCUIT_BREAKER,
+                parameters = listOf(
+                    InspectionParameter("Уровень продувки ф.А", "", "0.5-1"),
+                    InspectionParameter("Уровень продувки ф.В", "", "0.5-1"),
+                    InspectionParameter("Уровень продувки ф.С", "", "0.5-1"),
+                )
+            ),
+            Equipment(
+                id = "ТТ-220 Комета-2",
+                name = "ТТ-220 Комета-2",
+                type = EquipmentType.CURRENT_TRANSFORMER,
+                parameters = listOf(
+                    InspectionParameter("Уровень масла ф.А", "", ""),
+                    InspectionParameter("Уровень масла ф.В", "", ""),
+                    InspectionParameter("Уровень масла ф.С", "", ""),
+                )
+            ),
+            //Комета-1
+            Equipment(
+                id = "В-220 Комета-1",
+                name = "В-220 Комета-1",
+                type = EquipmentType.CIRCUIT_BREAKER,
+                parameters = listOf(
+                    InspectionParameter("Уровень продувки ф.А", "", "0.5-1"),
+                    InspectionParameter("Уровень продувки ф.В", "", "0.5-1"),
+                    InspectionParameter("Уровень продувки ф.С", "", "0.5-1"),
+                )
+            ),
+            Equipment(
+                id = "ТТ-220 Комета-1",
+                name = "ТТ-220 Комета-1",
+                type = EquipmentType.CURRENT_TRANSFORMER,
+                parameters = listOf(
+                    InspectionParameter("Уровень масла ф.А", "", ""),
+                    InspectionParameter("Уровень масла ф.В", "", ""),
+                    InspectionParameter("Уровень масла ф.С", "", ""),
+                )
+            ),
+            //Трансформаторы напряжения
+            Equipment(
+                id = "1ТН-220",
+                name = "1ТН-220",
+                type = EquipmentType.VOLTAGE_TRANSFORMER,
+                parameters = listOf(
+                    InspectionParameter("Уровень масла ф.А нижний каскад", "", ""),
+                    InspectionParameter("Уровень масла ф.А верхний каскад", "", ""),
+                    InspectionParameter("Уровень масла ф.В нижний каскад", "", ""),
+                    InspectionParameter("Уровень масла ф.В верхний каскад", "", ""),
+                    InspectionParameter("Уровень масла ф.С нижний каскад", "", ""),
+                    InspectionParameter("Уровень масла ф.С верхний каскад", "", ""),
+                )
+            ),
+            Equipment(
+                id = "2ТН-220",
+                name = "2ТН-220",
+                type = EquipmentType.VOLTAGE_TRANSFORMER,
+                parameters = listOf(
+                    InspectionParameter("Уровень масла ф.А нижний каскад", "", ""),
+                    InspectionParameter("Уровень масла ф.А верхний каскад", "", ""),
+                    InspectionParameter("Уровень масла ф.В нижний каскад", "", ""),
+                    InspectionParameter("Уровень масла ф.В верхний каскад", "", ""),
+                    InspectionParameter("Уровень масла ф.С нижний каскад", "", ""),
+                    InspectionParameter("Уровень масла ф.С верхний каскад", "", ""),
+                )
+            ),
+            Equipment(
+                id = "ТН-220 ОСШ",
+                name = "ТН-220 ОСШ",
+                type = EquipmentType.VOLTAGE_TRANSFORMER,
+                parameters = listOf(
+                    InspectionParameter("Уровень масла ф.В нижний каскад", "", ""),
+                    InspectionParameter("Уровень масла ф.В верхний каскад", "", ""),
+                )
+            ),
+
+        )
+    )
+
     val oru35 = Oru(
         voltage = "35",
         name = "ОРУ-35",
@@ -158,7 +412,7 @@ object SubstationData {
         )
     )
 
-    val allOru = listOf(oru500, oru35)
+    val allOru = listOf(oru500, oru220, oru35)
 
     fun getEquipmentGrouped(oru: Oru): Map<EquipmentType, List<Equipment>> {
         return oru.equipments.groupBy { it.type }
